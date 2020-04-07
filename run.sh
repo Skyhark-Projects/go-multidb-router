9b05a67873e63419dbe650e65d86745a3c801468
sacha vandamme <sacha@skyhark.be>
2020-04-08 01:33:31 +0200

    auto install vendor packages for testes

--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-clear && make test
+clear
+
+if [ ! -d ./vendor ]; then
+    export GOPATH=$(pwd)
+    mv vendor src || mkdir src
+
+    echo "installing vendor packages.."
+    go get github.com/jinzhu/gorm \
+           go.mongodb.org/mongo-driver/mongo \
+           github.com/go-sql-driver/mysql
+
+    mv src vendor
+    echo "vendor packages installed"
+fi
+
+make test
